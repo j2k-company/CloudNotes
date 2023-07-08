a4194faa870409883ce3f672ef7b057c35562f25
jaka2005 <jakalogin1808@gmail.com>
2023-07-08 19:25:49 +0400

    Init commit

---
+++ src/main/kotlin/site/j2k/plugins/Routing.kt
@@ -0,0 +1,9 @@
+package site.j2k.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    routing {
+    }
+}
---
+++ src/test/kotlin/site/j2k/ApplicationTest.kt
@@ -0,0 +1,5 @@
+package site.j2k
+
+
+class ApplicationTest {
+}
