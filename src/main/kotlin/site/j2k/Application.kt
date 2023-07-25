e7aa1fde1e8ecc9bd14410265d63d30d22e1535e
jaka2005 <jakalogin1808@gmail.com>
2023-07-25 22:03:08 +0400

    change server creation method

--- src/main/kotlin/site/j2k/Application.kt
+++ src/main/kotlin/site/j2k/Application.kt
@@ -1,14 +1,9 @@
 package site.j2k
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import site.j2k.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     configureRouting()
